da265ce93ebb9c8cbd4c3485e6fc3964cf27f98b
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2023-08-12 12:56:55 +0700

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      '80%:': {
+        opacity: 0.5,
+       },
+    },
   },
   plugins: [],
 }
